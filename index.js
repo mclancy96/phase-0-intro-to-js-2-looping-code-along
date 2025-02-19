0fdb1c5a6ef272a688579c89e807ca04fbd0fee7
Mike Clancy <mclancy96@gmail.com>
2025-02-19 16:46:51 -0500

    add writeCards

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solutions in this file
+const writeCards = (names, typeOfCard) => {
+    const messages = [];
+    names.forEach(name => {
+        messages.push(`Thank you, ${name}, for the wonderful ${typeOfCard} gift!`);
+    });
+    return messages;
+}
+
+const countDown = () => {
+
+}
